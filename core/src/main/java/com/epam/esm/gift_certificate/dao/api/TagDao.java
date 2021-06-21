69dc03c22cbc858db5829f31740a6933dbbc4667
ivan850502 <Ivan77.pristavkin@gmail.com>
2021-06-21 19:16:02 +0300

    Added javadoc

--- core/src/main/java/com/epam/esm/gift_certificate/dao/api/TagDao.java
+++ core/src/main/java/com/epam/esm/gift_certificate/dao/api/TagDao.java
@@ -8,16 +8,54 @@ import com.epam.esm.gift_certificate.model.entity.Tag;
 import java.util.List;
 import java.util.Optional;
 
-public interface TagDao<T extends Entity> {
+/**
+ * Dao for operating with Tag database
+ *
+ * @param <T> Any implementations of Tag entity
+ */
+public interface TagDao<T extends Tag> {
+    /**
+     * Retrieves list of tags from db
+     * @return List of Tags
+     */
     List<T> getAll();
 
+    /**
+     * Retrieves tag by id from db
+     * @param id
+     * @return Optional of Tag
+     * @throws NoSuchTagException if tag is not exist
+     */
     Optional<T> get(int id) throws NoSuchTagException;
 
+    /**
+     * Deletes tag by id from db
+     * @param id
+     * @throws NoSuchTagException if tag is not exist
+     */
     void delete(int id) throws NoSuchTagException;
 
+    /**
+     * Saves tag in db
+     * @param entity
+     * @return Optional of Tag
+     * @throws NoSuchTagException if tag is not exist
+     * @throws TagCreationException if there are any problems with saving
+     */
     Optional<T> create(T entity) throws NoSuchTagException, TagCreationException;
 
+    /**
+     * Retrieves tag by name from db
+     * @param name
+     * @return Optional of Tag
+     * @throws NoSuchTagException
+     */
     Optional<Tag> getTagByName(String name) throws NoSuchTagException;
 
+    /**
+     * Retrieves list of tags from db by GiftCertificate id
+     * @param giftCertificateId
+     * @return List of tags
+     */
     List<T> getTagsByGiftCertificateId(int giftCertificateId);
 }
